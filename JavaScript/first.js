1921773656fc399ba1ff8ba70c4ebfdb6494671c
shivam240104 <mauryatanmay845@gmail.com>
2025-09-11 15:28:38 +0530

    today learn javascript

---
+++ JavaScript/first.js
@@ -0,0 +1,48 @@
+console.log(a);
+var a;
+var b=6;
+console.log(a);
+{
+    var c=7;
+    let d=0;
+    console.log(b);
+}
+console.log(c);
+// console.log(d);
+
+function f1(user="guest"){
+    console.log(`Hello ${user}`);
+}
+f1();
+
+ function waitforTwoSecond(){
+    const ms = new Date().getTime()+2000;
+    while(new Date().getTime()<ms);
+}
+
+function register(){
+waitforTwoSecond();
+console.log("register end");
+}
+function sendEmail(){
+waitforTwoSecond();
+console.log("Email send");
+}
+function login(){
+waitforTwoSecond();
+console.log("Enter in the app");
+}
+function getData(){
+waitforTwoSecond();
+console.log("Data gotted");
+}
+function DisplayData(){
+waitforTwoSecond();
+console.log("other Application");
+}
+register();
+sendEmail();
+login();
+getData();
+DisplayData();
+console.log("hell0 wait");
